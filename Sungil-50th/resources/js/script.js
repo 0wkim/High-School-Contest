96326c3ba7c57feb0634c4138fe0c0de7eaf1b54
00woo04 <80682346+00woo04@users.noreply.github.com>
2022-12-23 21:10:41 +0900

    2022

---
+++ Sungil-50th/resources/js/script.js
@@ -0,0 +1,44 @@
+// slide
+$(".slide:gt(0)").hide()
+setInterval(function(){
+  $(".slide:first").fadeOut(1500).next().fadeIn(1500)
+  $(".slide:first").appendTo(".slideImg")
+}, 3000);
+
+
+
+// timeline
+$(".step").click( function() {
+	$(this).addClass("active").prevAll().addClass("active");
+	$(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+	$("#line-progress").css("width", "3%");
+	$(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+	$("#line-progress").css("width", "25%");
+	$(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+	$("#line-progress").css("width", "50%");
+	$(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+	$("#line-progress").css("width", "75%");
+	$(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
